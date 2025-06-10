08f4b5b807395533cf99eb0a8f60922b363d08a6
ideacity4 <ideacity4@gmail.com>
2025-06-10 09:48:57 +0530

    added avatar to user module

--- models/User.js
+++ models/User.js
@@ -5,6 +5,10 @@ const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: { type: String, unique: true ,required: true}, // It is Unique for each user
     password: {type: String, required: true}, // (This is Hashed Password)
+    avatar:{
+        type : String,
+        default: "https://www.svgrepo.com/show/384670/account-avatar-profile-user.svg"
+    },
     role: { type: String, enum: ["user", "admin"], required: true },
     created_at: { type: Date, default: Date.now }
   });
